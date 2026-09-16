dbb945f2452fb79d61358f68d3e2d53bcce46fe0
mianrabelwilfrid-afk <mianrabelwilfrid@gmail.com>
2026-09-16 15:28:19 +0100

    Add contact button alert functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+const contactButton =
+    document.getElementById("contactButton");
+
+contactButton.addEventListener("click", function () {
+
+    alert(
+        "Merci pour votre intérêt ! " +
+        "Le formulaire de contact sera bientôt disponible."
+    );
+
+});
